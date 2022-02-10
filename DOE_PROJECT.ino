65611751016b326c8b9ba5b563d6f9bc9d4ab845
markaustin017@gmail.com <markaustin017@gmail.com>
2022-02-10 23:28:55 +0800

    Add

--- DOE_PROJECT.ino
+++ DOE_PROJECT.ino
@@ -1,7 +1,5 @@
-
-// Fill-in information from your Blynk Template here
-#define BLYNK_TEMPLATE_ID "TMPLUVz-1bZJ"
-#define BLYNK_DEVICE_NAME "IoT based Robot Arm"
+#define BLYNK_TEMPLATE_ID "TMPLnxQhKedE"
+#define BLYNK_DEVICE_NAME "Design IoT"
 
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
 
@@ -11,160 +9,104 @@
 #define APP_DEBUG
 
 // Uncomment your board, or configure a custom board in Settings.h
-//#define USE_WROVER_BOARD
-//#define USE_TTGO_T7
-//#define USE_ESP32C3_DEV_MODULE
-//#define USE_ESP32S2_DEV_KIT
+//#define USE_SPARKFUN_BLYNK_BOARD
+#define USE_NODE_MCU_BOARD
+//#define USE_WITTY_CLOUD_BOARD
+//#define USE_WEMOS_D1_MINI
 
 #include "BlynkEdgent.h"
 
-//#include "ThingSpeak.h"
+#include "HX711.h"
 
-#include <Adafruit_MLX90614.h>
-#include <Wire.h>
-#include <ESP32Servo.h>
-//#include <WiFi.h>
+#define DOUT 2
+#define SCK 15
 
-#define SOUND_VELOCITY 0.034
-#define CM_TO_INCH 0.393701
+#define O3 D0
+#define O2 D1
+#define O1 D2
+#define O0 D3
 
-//BlynkTimer timer;
+#define waterSensor A0
 
-//WiFiClient client;
+HX711 scale;
 
-const int trigPin = 4;
-const int echoPin = 2;
+BlynkTimer timer;
 
-const int motor = 5;
+float calibration_factor = 419640;//142600 419640
 
-long duration;
-float distanceCm;
+int valNum;
 
-unsigned long myChannelNumber = 4;
-const char * myWriteAPIKey = "EVTYQ4UGMQBRD5FD";
-
-Servo temp1;
-Servo temp2;
-Servo servo1; 
-Servo servo2;
-Servo servo3; 
-Servo servoT;
-
-Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-
-BLYNK_WRITE(V1)
-{
-  int val = param.asInt(); // assigning incoming value from pin V1 to a variable
-  Serial.print("Servo 1: ");
-  Serial.print(val);
-  Serial.println("");
-  servo1.write(val);
-  sonicRead();
-}
+long last_sensor_reading = 0;
 
 BLYNK_WRITE(V2)
 {
-  int val = param.asInt(); // assigning incoming value from pin V1 to a variable
-  Serial.print("Servo 2: ");
-  Serial.print(val);
-  Serial.println("");
-  servo2.write(val);
-  sonicRead();
+  int timeVal = param.asInt(); // assigning incoming value from pin V1 to a variable
+  Blynk.virtualWrite(V4,convertVal(timeVal,0));
+  sendCommand(timeVal,0);
 }
 
 BLYNK_WRITE(V3)
 {
-  int val = param.asInt(); // assigning incoming value from pin V1 to a variable
-  Serial.print("Servo 3: ");
-  Serial.print(val);
-  Serial.println("");
-  servo3.write(val);
-  sonicRead();
-}
-
-BLYNK_WRITE(V4)
-{
-  int val = param.asInt(); // assigning incoming value from pin V1 to a variable
-  Serial.print("Servo 4: ");
-  Serial.print(val);
-  Serial.println("");
-  servoT.write(val);
-  sonicRead();
-}
-
-BLYNK_WRITE(V6)
-{
-  int val = param.asInt();
-  Serial.print("Dispense: ");
-  Serial.println(val);
-  if(val == 1){
-    setMotor(true);
-    delay(3000);
-    setMotor(false);
-  }
-}
-
-
-BLYNK_WRITE(V7)
-{
-  int val = param.asInt();
-  if(val == 1){
-    
-    Blynk.virtualWrite(V0,mlx.readObjectTempC());
-    //int x = ThingSpeak.writeField(myChannelNumber, 1, mlx.readObjectTempC(), myWriteAPIKey);
-  }
+  int weightVal = param.asInt(); // assigning incoming value from pin V1 to a variable
+  Blynk.virtualWrite(V5,convertVal(weightVal,1));
+  sendCommand(weightVal,1);
 }
 
 void setup()
 {
   Serial.begin(115200);
   delay(100);
-  temp1.attach(23);
-  temp2.attach(19);
-  servo1.attach(15); 
-  servo2.attach(16);
-  servo3.attach(17);
-  servoT.attach(18);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  pinMode(motor, OUTPUT);
-  mlx.begin(); 
-  //ThingSpeak.begin(client);  // Initialize ThingSpeak
+  
+  scale.begin(DOUT, SCK);
+  pinMode(O3, OUTPUT);
+  pinMode(O2, OUTPUT);
+  pinMode(O1, OUTPUT);
+  pinMode(O0, OUTPUT);
+  pinMode(waterSensor, INPUT);
+  timer.setInterval(500L,getWeight);
   BlynkEdgent.begin();
-  //timer.setInterval(500L,sonicRead);
 }
 
 void loop() {
+  timer.run();
   BlynkEdgent.run();
 }
 
-void sonicRead(){
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance
-  distanceCm = duration * SOUND_VELOCITY/2;
-  
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance (cm): ");
-  Serial.println(distanceCm);
-
-  Blynk.virtualWrite(V5,distanceCm);
+void getWeight(){
+  scale.tare(); //Reset the scale to 0
+  scale.set_scale(calibration_factor);
+  float val = scale.get_units(5);
+  Blynk.virtualWrite(V0,val);
 }
 
-void setMotor(bool x){
-  if(x == true){
-    digitalWrite(motor, HIGH);
+void sendCommand(int n,int select){
+  int a[3] = {0,0,0}, i;
+  for(i=0; n>0; i++){
+    a[i]=n%2;
+    n=n/2;
   }
-  else{
-    digitalWrite(motor, LOW);
+  
+  digitalWrite(O3,select);
+  digitalWrite(O2,a[2]);
+  digitalWrite(O1,a[1]);
+  digitalWrite(O0,a[0]);
+
+  Serial.println("");
+  Serial.print(select);
+  Serial.print(a[2]);
+  Serial.print(a[1]);
+  Serial.print(a[0]);
+  Serial.println("");
+}
+
+int convertVal(int x, int select){
+  if(select == 0){
+    if(x == 0)
+      return 0; 
+    else
+      return 24 / x;
+  }
+  else if(select == 1){
+    return 400 * x;
   }
 }
